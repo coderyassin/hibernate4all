eda2ef012e1c074fd1abb50c48f196e17afefd0c
MelloukiYassin <mellouki1997yassin@gmail.com>
2022-03-06 09:49:32 +0100

    initial commit

---
+++ src/main/java/com/yascode/hibernate4all/Hibernate4allApplication.java
@@ -0,0 +1,13 @@
+package com.yascode.hibernate4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate4allApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hibernate4allApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yascode/hibernate4all/Hibernate4allApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yascode.hibernate4all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hibernate4allApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
